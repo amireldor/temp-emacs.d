83a50b5c59430b8f535c88ae1b0590f0750e9eae
Amir Eldor <amir.eldor@gmail.com>
2018-03-16 15:38:59 +0200

    Initial frame width/height

--- amir/amir-customize.el
+++ amir/amir-customize.el
@@ -4,9 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-file "~/.emacs.d/amir/amir-customize.el")
+ '(default-frame-alist
+    (quote
+     ((horizontal-scroll-bars)
+      (width . 121)
+      (height . 47)
+      (vertical-scroll-bars))))
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (ace-jump-mode auto-complete)))
- '(special-display-frame-alist (quote ((height . 24) (width . 110) (unsplittable . t)))))
+ '(package-selected-packages (quote (ace-jump-mode auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
