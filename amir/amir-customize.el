0e3513ac351336afdc0cd9b58e2c91a0ac97a245
Amir Eldor <amir@eize.ninja>
2018-03-17 00:17:32 +0200

    Don't hardcode custom-set-variables file's path

--- amir/amir-customize.el
+++ amir/amir-customize.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-file "~/.emacs.d/amir/amir-customize.el")
+ '(custom-file (concat user-emacs-directory
+                       (convert-standard-filename "amir/amir-customize.el")))
  '(default-frame-alist
     (quote
      ((horizontal-scroll-bars)
